6fa1ec9fa7d487cb2cc895e7667436ad0df51dac
Kosmik <malpa.w.klapkach@gmail.com>
2025-04-09 13:07:58 +0200

    Add basic player movement

---
+++ Assets/_Project/Scripts/FPPViewRotation.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AE
+{
+    public class FPPViewRotation : MonoBehaviour
+    {
+        [Header("To Link")]
+        [SerializeField]
+        private Transform head;
+        [SerializeField]
+        private Transform body;
+
+        [SerializeField]
+        private InputActionReference lookInputAction; 
+        private InputAction lookInput;
+
+        [Header("Properties")]
+        [SerializeField]
+        public Vector2 sensitivity = 1.8f * Vector2.one;
+
+        [SerializeField]
+        private float minPitchAngle = -90;
+        [SerializeField]
+        private float maxPitchAngle = 90;
+
+        [Header("States")]
+        [SerializeField]
+        private float headPitchAngle = 0f;
+
+        private void Reset()
+        {
+            TryAssignBody();
+            TryAssignHead();
+
+            void TryAssignBody()
+            {
+                Component body = GetComponentInParent<Rigidbody>();
+                if (body)
+                {
+                    this.body = body.transform;
+                    return;
+                }
+
+                body = GetComponentInParent<CharacterController>();
+                if (body)
+                {
+                    this.body = body.transform;
+                    return;
+                }
+            }
+
+            void TryAssignHead()
+            {
+                if (body != transform)
+                {
+                    head = transform;
+                    return;
+                }
+
+                head = transform.Find("Head");
+                if (head == null)
+                    head = transform.Find("Camera");
+            }
+        }
+
+        private void OnEnable()
+        {
+            lookInput = lookInputAction.action.Clone();
+            lookInput.Enable();
+        }
+
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            Vector2 look = lookInput.ReadValue<Vector2>();
+            look.Scale(sensitivity);
+
+            headPitchAngle = Mathf.Clamp(headPitchAngle - look.y, minPitchAngle, maxPitchAngle);
+
+            head.transform.localRotation = Quaternion.AngleAxis(headPitchAngle, Vector3.right);
+            body.Rotate(Vector3.up * look.x);
+        }
+
+        private void OnDisable()
+        {
+            lookInput.Disable();
+            lookInput.Dispose();
+            lookInput = null;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AE
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class SimplePlayerMovement : MonoBehaviour
+    {
+        private CharacterController characterController;
+
+        [SerializeField]
+        private float moveSpeed = 4;
+
+        [SerializeField]
+        private InputActionReference moveInputAction;
+        private InputAction moveInput;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        private void OnEnable()
+        {
+            moveInput = moveInputAction.action.Clone(); 
+            moveInput.Enable();
+            characterController.enabled = true;
+        }
+
+        private void Update()
+        {
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            Vector2 input = moveInput.ReadValue<Vector2>();
+            Vector3 direction = transform.forward * input.y + transform.right * input.x;
+            direction.y = 0;
+
+            characterController.SimpleMove(moveSpeed * direction);
+        }
+     
+        private void OnDisable()
+        {
+            characterController.enabled = false;
+            moveInput.Disable();
+            moveInput.Dispose();
+            moveInput = null;
+        }
+    }
+}
